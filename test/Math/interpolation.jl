87dba6bd79558de725385967ad24d78f86bf48de
MicheleCeresoli <michele.ceresoli7@gmai.com>
2023-05-17 15:48:15 +0200

    Updated Interpolation tests

--- test/Math/interpolation.jl
+++ test/Math/interpolation.jl
@@ -79,6 +79,88 @@
     y = jMath.interpolate(cs, xn[end] + 1, true)
     @test y ≈ [yn[end], zn[end]] atol = 1e-11 rtol = 1e-11
 
+    # Test permutations 
+    xn = [2, 1, 3, 0, 5]
+    yn = sin.(xn)
+    zn = cos.(xn)
+
+    cs = JSMDUtils.Math.InterpCubicSplines(xn, yn)
+    @test cs.xn == [0, 1, 2, 3, 5]
+    @test cs.yn[:] ≈ sin.([0, 1, 2, 3, 5]) atol=1e-11 rtol=1e-11
+
+    cs = JSMDUtils.Math.InterpCubicSplines(xn, hcat(yn, zn)')
+    @test cs.xn == [0, 1, 2, 3, 5]
+    @test cs.yn[1, :] ≈ sin.([0, 1, 2, 3, 5]) atol=1e-11 rtol=1e-11
+    @test cs.yn[2, :] ≈ cos.([0, 1, 2, 3, 5]) atol=1e-11 rtol=1e-11
+
+    # Test wrong dimensions
     yn = rand(3, 3, 3)
     @test_throws ArgumentError JSMDUtils.Math.InterpCubicSplines(xn, yn)
-end
+end;
+
+@testset "Akima" verbose = true begin 
+
+    xn = LinRange(0, 2 * π, 10)
+    yn = sin.(xn)
+
+    ak = JSMDUtils.Math.InterpAkima(xn, yn)
+
+    # Test node values
+    for j in eachindex(xn)
+        @test jMath.interpolate(ak, xn[j]) ≈ yn[j] atol = 1e-11 rtol = 1e-11
+    end
+
+    df = x -> jMath.interpolate(ak, x, false)
+
+    # Test 1st derivatives to be continuous
+    for j in 2:(length(xn) - 1)
+        @test D¹(df, xn[j] - 1e-15) ≈ D¹(df, xn[j] + 1e-15) atol = 1e-11 rtol = 1e-11
+    end
+
+    # Test FLAT extrapolation
+    fcn = x -> jMath.interpolate(ak, x, true)
+
+    for (x, y) in zip((xn[1] - 3, xn[end] + 3), (yn[1], yn[end]))
+        # Value should be equal to that of the last point
+        @test fcn(x) ≈ y atol = 1e-11 rtol = 1e-11
+
+        # Derivatives should all be null
+        for d in (D¹, D², D³)
+            @test d(fcn, x) == 0
+        end
+    end
+
+    # Test for N-dimensional Akima splines 
+    zn = cos.(xn)
+    ak = JSMDUtils.Math.InterpAkima(xn, hcat(yn, zn)')
+
+    # Test node values
+    for j in eachindex(xn)
+        y = jMath.interpolate(ak, xn[j])
+        @test length(y) == 2
+        @test y ≈ [yn[j], zn[j]] atol = 1e-11 rtol = 1e-11
+    end
+
+    # Test FLAT N-dimensional extrapolation 
+    y = jMath.interpolate(ak, xn[1] - 1, true)
+    @test y ≈ [yn[1], zn[1]] atol = 1e-11 rtol = 1e-11
+    
+    # Test permutations 
+    xn = [2, 1, 3, 0, 5]
+    yn = sin.(xn)
+    zn = cos.(xn)
+
+    ak = JSMDUtils.Math.InterpAkima(xn, yn)
+    @test ak.xn == [0, 1, 2, 3, 5]
+    @test ak.yn[:] ≈ sin.([0, 1, 2, 3, 5]) atol=1e-11 rtol=1e-11
+
+    ak = JSMDUtils.Math.InterpAkima(xn, hcat(yn, zn)')
+    @test ak.xn == [0, 1, 2, 3, 5]
+    @test ak.yn[1, :] ≈ sin.([0, 1, 2, 3, 5]) atol=1e-11 rtol=1e-11
+    @test ak.yn[2, :] ≈ cos.([0, 1, 2, 3, 5]) atol=1e-11 rtol=1e-11
+
+    # Test wrong dimensions
+    yn = rand(3, 3, 3)
+    @test_throws ArgumentError JSMDUtils.Math.InterpAkima(xn, yn)
+
+end;
