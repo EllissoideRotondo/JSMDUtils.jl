12ae8a4684de865456364c4015cd3a965fddc152
Andrea Pasquale <andrea.pasquale@polimi.it>
2023-06-18 12:04:40 +0200

    additional linalg utils

--- src/Math/linalg.jl
+++ src/Math/linalg.jl
@@ -1,4 +1,9 @@
-export skew
+export skew, 
+       unitvec, 
+       unitcross, 
+       projvec, projplane, 
+       anglevec, anglevecd,
+       angleplane, angleplaned
 
 @inbounds function skew(v::AbstractArray{N}) where {N<:Number}
     @assert length(v) == 3
@@ -15,3 +20,88 @@ end
 Compute a skew-symmetric matrix from the vector `v`. 
 """
 skew
+
+"""
+    unitvec(v::AbstractArray)
+
+Compute the normalized vector of `v`.
+"""
+function unitvec(v::AbstractArray)
+    nv = norm(v)
+    if nv == 1.
+        return v
+    else 
+        return v/nv
+    end
+end
+
+"""
+    unitcross(v1::AbstractArray, v2::AbstractArray)
+
+Compute the normalized cross product of `v1` and `v2`.
+"""
+function unitcross(v1::AbstractArray, v2::AbstractArray)
+    tmp = cross(v1, v2)
+    return unitvec(tmp)
+end
+
+"""
+    projvec(v1::AbstractArray, v2::AbstractArray)
+
+Compute the orthogonal projection of a vector `v1` on a vector `v2`.
+"""
+function projvec(v1::AbstractArray, v2::AbstractArray)
+    v2n2 = dot(v2, v2)
+    if v2n2 == 0.
+        return SVector{3}(0., 0., 0.)
+    end
+    return dot(v1, v2) * v2/v2n2
+end
+
+"""
+    projplane(v1::AbstractArray, n::AbstractArray)
+
+Compute the orthogonal projection of a vector `v1` on a plane with normal `n`.
+"""
+function projplane(v1::AbstractArray, n::AbstractArray)
+    return v1 - projvec(v1, n)
+end
+
+"""
+    anglevec(v1::AbstractArray, v2::AbstractArray)
+
+Compute the angle between two vectors `v1` and `v2`, in rad.
+"""
+function anglevec(v1::AbstractArray, v2::AbstractArray)
+    tmp = dot(unitvec(v1), unitvec(v2))
+    return mod(acos(tmp), π)
+end
+
+"""
+    anglevecd(v1::AbstractArray, v2::AbstractArray)
+
+Compute the angle between two vectors `v1` and `v2`, in deg.
+"""
+function anglevecd(v1::AbstractArray, v2::AbstractArray)
+    return rad2deg(anglevec(v1, v2))
+end
+
+"""
+    angleplane(v1::AbstractArray, n::AbstractArray)
+
+Compute the angle between a vector `v1` and (its projection on) a plane with
+normal `n`, in rad. 
+"""
+function angleplane(v1::AbstractArray, n::AbstractArray)
+    return anglevec(projplane(v1, n), v1)
+end
+
+"""
+    angleplaned(v1::AbstractArray, n::AbstractArray)
+
+Compute the angle between a vector `v1` and (its projection on) a plane with
+normal `n`, in deg. 
+"""
+function angleplaned(v1::AbstractArray, n::AbstractArray)
+    return rad2deg(angleplane(v1, n))
+end
--- test/Math/linalg.jl
+++ test/Math/linalg.jl
@@ -1,4 +1,4 @@
-using JSMDUtils.Math: skew
+using JSMDUtils.Math
 using LinearAlgebra
 
 @testset "Skew-matrix" begin
@@ -12,4 +12,66 @@ using LinearAlgebra
     @test S[3, 1] == -2 
     @test S[3, 2] == 1
 
+end
+
+@testset "unitvec" begin
+    I = [1., 0., 0.]
+    J = [0., 1., 0.]
+    K = [0., 0., 1.]
+    @test unitvec(I) == I
+    @test unitvec(J) == J
+    @test unitvec(K) == K
+
+    v = rand(3)
+    vn = norm(v)
+    @test unitvec(v) == v/vn
+end
+
+@testset "unitcross" begin 
+    I = [1., 0., 0.]
+    J = [0., 1., 0.]
+    K = [0., 0., 1.]
+    @test unitcross(I, J) == K
+
+    v1 = [2., 0., 0.]
+    @test unitcross(v1, J) == K 
+end
+
+@testset "projvec" begin 
+    I = [1., 0., 0.]
+    J = [0., 1., 0.]
+    @test projvec(I, J) == zeros(3)
+
+    v = [cosd(30), sind(30), 0.]
+    @test projvec(v, I) == [v[1], 0., 0.]
+    @test projvec(v, J) == [0., v[2], 0.]
+
+    n = [0., 0., 1.]
+    @test projplane(I, n) == I
+    @test projplane(J, n) == J
+    @test projplane(v, n) == v
+
+    v = [cosd(30)*cosd(45), sind(30)*cosd(45), sind(45)]
+    @test projplane(v, n) == [v[1], v[2], 0.]
+end
+
+@testset "anglevec/anglevecd" begin
+    I = [1., 0., 0.]
+    J = [0., 1., 0.]
+    @test anglevec(I, J) == π/2
+    @test anglevec(I, -J) == π/2
+    @test anglevec(-I, -J) == π/2
+
+    v = [cosd(30), sind(30), 0.]
+    @test anglevec(I, v) ≈ π/6
+    @test anglevec(v, I) ≈ π/6
+    @test anglevecd(J, v) ≈ 60.
+end
+
+@testset "angleplane/angleplaned" begin
+    
+    v = [cosd(30)*cosd(45), sind(30)*cosd(45), sind(45)]
+    @test angleplane(v, [0., 0., 1.]) ≈ π/4
+    @test angleplaned(v, [0., 0., 1.]) ≈ 45.
+    
 end
