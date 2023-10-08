5568716f7965f7d165849c73c64bb8f6cbf61bfe
MicheleCeresoli <85893254+MicheleCeresoli@users.noreply.github.com>
2023-10-08 22:24:37 +0200

    Added tests for cross3

--- test/Math/linalg.jl
+++ test/Math/linalg.jl
@@ -73,19 +73,27 @@ end
         θ = rand()
         a, b = get_vector(θ), get_vector2(θ)
 
-        for (i, fcn) in enumerate([Math.cross6, Math.cross9, Math.cross12])
+        for (i, fcn) in enumerate([Math.cross3, Math.cross6, Math.cross9, Math.cross12])
             cp = fcn(a, b)
 
             @test cp[1:3] ≈ cross(a[1:3], b[1:3]) atol = atol # Cross product 
-            @test cp[4:6] ≈ D¹(δcross, θ) atol = atol # 1st order derivative   
+            
+            if i > 1 
+                # 1st order derivative   
+                @test cp[4:6] ≈ D¹(δcross, θ) atol = atol 
 
-            if i > 1 # 2nd order derivative 
-                @test cp[7:9] ≈ D²(δcross, θ) atol = atol
+                if i > 2 
+                    # 2nd order derivative 
+                    @test cp[7:9] ≈ D²(δcross, θ) atol = atol
+
+                    if i > 3 
+                        # 3rd order derivative 
+                        @test cp[10:12] ≈ D³(δcross, θ) atol = atol
+                    end
+                end
+
             end
 
-            if i > 2 # 3rd order derivative 
-                @test cp[10:12] ≈ D³(δcross, θ) atol = atol
-            end
         end 
     end
 end
